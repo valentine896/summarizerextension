fc5493ec3941d37951bba856f2a4afabb5a5b335
valentine896 <amunonye@yahoo.com>
2023-06-02 21:38:12 -0500

    Initial commit

---
+++ api/summarize.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+
+const apiKey = process.env.SUMMARIZER_API_KEY;
+
+const calculateMaxTokens = (inputText) => {
+  const inputTokens = inputText.split(' ').length;
+  const estimatedSummaryTokens = Math.ceil(inputTokens * 0.2);
+  return Math.min(estimatedSummaryTokens, 6000);
+};
+
+module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  console.log('Received request to /summarize');
+  const { text } = req.body;
+  console.log('Text to summarize:', text);
+  const maxTokens = calculateMaxTokens(text);
+
+  try {
+    console.log('Sending request to GPT-3 API...');
+    const gpt3Response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: text,
+          },
+          {
+            role: "system",
+            content: "Please provide a 5-7 sentence summary of the above text that captures the main idea and conclusion in first person.",
+          },
+        ],
+        max_tokens: maxTokens,
+        temperature: 0,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+
+    console.log('Received response from GPT-3 API');
+
+    if (gpt3Response.data.choices && gpt3Response.data.choices.length > 0) {
+      const summary = gpt3Response.data.choices[0].message.content.trim();
+      console.log('GPT-3 API response:', gpt3Response.data);
+      console.log('Generated summary:', summary);
+      res.status(200).json({ summary });
+    } else {
+      console.log('No choices found in GPT-3 API response:', gpt3Response.data);
+      res.status(500).json({ error: 'No summary generated' });
+    }
+  } catch (error) {
+    console.error('Error calling GPT-3 API:', error);
+    res.status(500).json({ error: `Error calling GPT-3 API: ${error.message}` });
+  }
+};
---
+++ popup.js
@@ -0,0 +1,85 @@
+document.getElementById('summarizeBtn').addEventListener('click', async () => {
+  console.log('Summarize button clicked...');
+  const selectedText = await getSelectedText();
+  console.log('Selected text:', selectedText);
+
+  if (selectedText) {
+        console.log('Calling fetchSummary...');  // Added this line
+    try {
+      const summary = await fetchSummary(selectedText);
+      console.log('Summary:', summary);
+      // Replace this with the code to display the summary in your extension
+    } catch (error) {
+      console.error('Error fetching summary:', error);
+    }
+  } else {
+    console.log('No text selected.');
+  }
+});
+
+async function getSelectedText() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (!tabs[0] || tabs[0].url.startsWith('chrome://')) {
+        console.log('No active web page tab found.');
+        resolve('');
+        return;
+      }
+
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          function: () => window.getSelection().toString(),
+        },
+        (result) => {
+          if (chrome.runtime.lastError) {
+            console.error('Error executing script:', chrome.runtime.lastError);
+            resolve('');
+            return;
+          }
+
+          if (!result || !Array.isArray(result) || result.length === 0) {
+            console.log('No result from executeScript.');
+            resolve('');
+            return;
+          }
+
+          console.log('Result from executeScript:', result[0].result);
+          resolve(result[0].result);
+        }
+      );
+    });
+  });
+}
+
+// Rest of your code...
+
+async function fetchSummary(text) {
+  console.log('Fetching summary for text:', text);  // Added this line
+  const response = await fetch('https://clientside-jdv7dpgcg-vvv1995-yahoocom.vercel.app/api/summarize', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text }),
+  });
+
+  console.log('Response:', response);  // Added this line
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch summary: ${response.statusText}`);
+  }
+
+  let responseData;
+  try {
+    responseData = await response.json();
+  } catch (error) {
+    console.error('Error parsing response as JSON:', error);
+    throw new Error('Unexpected server response');
+  }
+
+  console.log('Response data:', responseData);  // Added this line
+
+  const { summary } = responseData;
+  return summary;
+}
