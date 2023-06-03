be80a0dd38901ffd93e3009d2bde34b307d27804
valentine896 <amunonye@yahoo.com>
2023-06-02 22:18:31 -0500

    cors

--- api/summarize.js
+++ api/summarize.js
@@ -11,8 +11,13 @@ const calculateMaxTokens = (inputText) => {
 module.exports = async (req, res) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
   console.log('Received request to /summarize');
   const { text } = req.body;
